2ba0d5fb47cf84d00f89c1f39224ea86ef5134ab
Nguyễn Trung Nhân <52454131+nhanntdl@users.noreply.github.com>
2025-03-15 22:26:23 +0700

    Remove unused CustomWidgetElement and its export from flex_markdown.dart

--- lib/flex_markdown.dart
+++ lib/flex_markdown.dart
@@ -6,4 +6,3 @@ export 'src/parser.dart';
 export 'src/widgets/form_elements.dart';
 export 'src/models/form_field_configurations.dart';
 export 'src/models/controller_button_configurations.dart';
-export 'src/models/widget_element.dart'; // Add this export
